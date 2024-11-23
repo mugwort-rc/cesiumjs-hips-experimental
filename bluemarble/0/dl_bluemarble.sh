0c7cabe15ae1ddd48e9d818731617697c53a370f
mugwort_rc <mugwort.rc@gmail.com>
2024-11-24 04:06:12 +0900

    Add bluemarble

---
+++ bluemarble/0/dl_bluemarble.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+wget https://alasky.cds.unistra.fr/Planets/CDS_P_Earth_BlueMarble/Norder0/Dir0/Npix0.jpg -O 0.jpg
+wget https://alasky.cds.unistra.fr/Planets/CDS_P_Earth_BlueMarble/Norder0/Dir0/Npix1.jpg -O 1.jpg
+wget https://alasky.cds.unistra.fr/Planets/CDS_P_Earth_BlueMarble/Norder0/Dir0/Npix2.jpg -O 2.jpg
+wget https://alasky.cds.unistra.fr/Planets/CDS_P_Earth_BlueMarble/Norder0/Dir0/Npix3.jpg -O 3.jpg
+wget https://alasky.cds.unistra.fr/Planets/CDS_P_Earth_BlueMarble/Norder0/Dir0/Npix4.jpg -O 4.jpg
+wget https://alasky.cds.unistra.fr/Planets/CDS_P_Earth_BlueMarble/Norder0/Dir0/Npix5.jpg -O 5.jpg
+wget https://alasky.cds.unistra.fr/Planets/CDS_P_Earth_BlueMarble/Norder0/Dir0/Npix6.jpg -O 6.jpg
+wget https://alasky.cds.unistra.fr/Planets/CDS_P_Earth_BlueMarble/Norder0/Dir0/Npix7.jpg -O 7.jpg
+wget https://alasky.cds.unistra.fr/Planets/CDS_P_Earth_BlueMarble/Norder0/Dir0/Npix8.jpg -O 8.jpg
+wget https://alasky.cds.unistra.fr/Planets/CDS_P_Earth_BlueMarble/Norder0/Dir0/Npix9.jpg -O 9.jpg
+wget https://alasky.cds.unistra.fr/Planets/CDS_P_Earth_BlueMarble/Norder0/Dir0/Npix10.jpg -O 10.jpg
+wget https://alasky.cds.unistra.fr/Planets/CDS_P_Earth_BlueMarble/Norder0/Dir0/Npix11.jpg -O 11.jpg
